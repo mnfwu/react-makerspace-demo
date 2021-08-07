12b4a6c03bd105f13302eb66109d76c5b1222f04
Matthew Wu <mnfwu12@gmail.com>
2021-08-07 16:03:55 +0800

    adjust index copy

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
             <Nav />
             <div className="top">
                 
-                <h1 className="h1">HOUSE OF CIRCLES</h1>
+                <h1 className="h1">DOT SPACE</h1>
                 <div className="circle-container">
                     <div className="circle-large grn-bg"></div>
                     <div className="circle-large"><Icon icon="eos-icons:rotating-gear" className="gear ylw-txt" /></div>
@@ -17,7 +17,7 @@ const Header = () => {
                 </div>
             </div>
             <div className="bottom">
-                <h4 className="bottom-text">House of Circles is a gallery space in Berlin exploring the history of polka dots in art, fashion, music, and culture.</h4>
+                <h4 className="bottom-text">Dot space is a community-run makerspace in Shanghai providing local creative with facility rental, workshops and exhibition space.</h4>
             </div>
         </header>
     )
