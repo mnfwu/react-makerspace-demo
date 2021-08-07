0843b361a12512187e4997817519c46692f5dca7
Tina Shan <tinashan22@gmail.com>
2021-08-07 22:47:53 +0800

    styled mobile menu button

--- src/components/Nav/NavButton.jsx
+++ src/components/Nav/NavButton.jsx
@@ -7,8 +7,13 @@ const NavButton = () => {
     let [active, toggleActive] = useState('inactive');
 
     return (
-        <div onClick={()=> toggleActive(active === 'inactive' ? active = 'active' : active = 'inactive')} className="grn-bg circle mobile-show">
-            <div>
+        <div onClick={()=> toggleActive(active === 'inactive' ? active = 'active' : active = 'inactive')} className="mobile-show">
+            <div className={`float-${active}`}>
+                <div className="menu-float blu-bg">
+                floating menu
+            </div>
+            </div>
+            <div className="grn-bg circle">
                 <div className={active}>
                  <Icon className="icon-screw" icon={screwIcon} />
                  </div>
