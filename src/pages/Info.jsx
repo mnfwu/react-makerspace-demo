5cb16227eb5439c0b6c6cca4b9bbd422ebe3c061
Tina Shan <tinashan22@gmail.com>
2021-08-08 23:15:23 +0800

    fixed mobile nav

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -7,7 +7,7 @@ const Info = () => {
         <div className="App">
             <Nav />
             <div className="blk-txt info-container">
-                <h2 className="mb3">INFORMATION</h2>
+                <h2 className="mb3 h2">INFORMATION</h2>
                 <p>Dot space is a community-run makerspace in Shanghai. Our memebrs share and manage a space that provides laser cutting, printmaking, 3D printing,  wood working, soldering and sewing facilities.</p>
                 <p>We are located at 98 Suzhou Lu, Huangpu District. </p>
                 <p>Check out events page for weekly workshops led by members and monthly open house tours.</p>
